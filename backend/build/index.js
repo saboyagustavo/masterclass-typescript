4968c6e4dd5a90740b5bd50e4f9e1ebdd047c843
Gustavo Saboya <saboya.gustavo@gmail.com>
2021-05-10 13:58:50 -0300

    Configured CORS and transpiled API to run the server.

--- backend/build/index.js
+++ backend/build/index.js
@@ -4,8 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var cors_1 = __importDefault(require("cors"));
+var routes_1 = __importDefault(require("./routes"));
 var app = express_1.default();
-app.get('/', function (request, response) {
-    return response.send('Hello world!');
-});
-app.listen(3333, function () { return 'SERVER RUNNING ON PORT 3333'; });
+app.use(cors_1.default());
+app.use(routes_1.default);
+app.listen(3333, function () { return console.log('SERVER RUNNING ON PORT 3333'); });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(routes);
 
 
-app.listen(3333, () => 'SERVER RUNNING ON PORT 3333');
+app.listen(3333, () => console.log('SERVER RUNNING ON PORT 3333'));
